1092996f22f1b2f98b83e8177068b4a002e693a5
cjltsod <github.tsod@tsod.idv.tw>
2019-03-09 02:29:13 +0800

    Add backstage

--- estore/views.py
+++ estore/views.py
@@ -8,8 +8,14 @@ from .models import Product
 # Create your views here.
 
 
-class ProductList(generic.ListView):
+class ProductList(PermissionRequiredMixin, generic.ListView):
     model = Product
+
+    def has_permission(self):
+        if self.permission_required:
+            return super(ProductList, self).has_permission()
+        else:
+            return True
 
 
 class ProductDetail(generic.DetailView):
@@ -23,7 +29,7 @@ class ProductCreate(PermissionRequiredMixin, generic.CreateView):
 
     def get_success_url(self):
         messages.success(self.request, '產品已新增')
-        return reverse('product_list')
+        return reverse('dashboard_product_list')
 
 
 class ProductUpdate(PermissionRequiredMixin, generic.UpdateView):
@@ -33,4 +39,4 @@ class ProductUpdate(PermissionRequiredMixin, generic.UpdateView):
 
     def get_success_url(self):
         messages.success(self.request, '產品已變更')
-        return reverse('product_update', kwargs=self.kwargs)
+        return reverse('dashboard_product_update', kwargs=self.kwargs)
