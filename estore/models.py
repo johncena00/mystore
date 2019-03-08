22cbead6ce9307ab55974b71cde8fe2280dca683
cjltsod <github.tsod@tsod.idv.tw>
2019-03-09 05:29:43 +0800

    Let cart item quantity can not less than 1

--- estore/models.py
+++ estore/models.py
@@ -11,7 +11,7 @@ from django_fsm import FSMField, transition
 class Product(models.Model):
     title = models.CharField(max_length=255, verbose_name='產品名稱')
     description = models.TextField(verbose_name='產品敘述')
-    quantity = models.IntegerField(verbose_name='庫存數量')
+    quantity = models.PositiveIntegerField(verbose_name='庫存數量')
     price = models.IntegerField(verbose_name='價格')
     image = models.ImageField(upload_to='product_images/', verbose_name='商品圖片', blank=True, null=True)
 
