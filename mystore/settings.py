eb8dd5f687159463d102e0e9a87e900fbd9ea8a3
rockwang <devilcry4@gmail.com>
2019-05-12 16:55:15 +0800

    Add product image

--- mystore/settings.py
+++ mystore/settings.py
@@ -124,3 +124,6 @@ STATIC_URL = '/static/'
 LOGIN_REDIRECT_URL = '/'
 LOGIN_URL = '/login/'
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
