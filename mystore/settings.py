3eb70408f0bf1239146fbea1c56b8752922a7e01
rockwang <devilcry4@gmail.com>
2019-05-21 16:06:37 +0800

    add i18n

--- mystore/settings.py
+++ mystore/settings.py
@@ -108,11 +108,11 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'zh-Hant'
 
 TIME_ZONE = 'UTC'
 
-USE_I18N = True
+USE_I18N = False
 
 USE_L10N = True
 
